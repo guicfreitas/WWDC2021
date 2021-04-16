4bc0092615743eeb3befe20058024e37b731d37d
guilherme <jguicf@gmail.com>
2021-04-16 16:30:51 -0400

    feat: add assets

---
+++ Let’s Rock!.playgroundbook/Edits/UserEdits.diffpack/UserModules/InitialView.playgroundmodule/Sources/InitialView.swift
@@ -0,0 +1,66 @@
+#imageLiteral(resourceName: "country.png")#imageLiteral(resourceName: "folk.png")#imageLiteral(resourceName: "jazz.png")#imageLiteral(resourceName: "nextPicker.png")#imageLiteral(resourceName: "noPick.png")#imageLiteral(resourceName: "r&b.png")import SwiftUI
+import PlaygroundSupport
+public struct ContentView: View {
+    
+    @State var isHoverYes = false
+    @State var isHoverNo = false
+    
+    public init(){
+        
+    }
+    public var body: some View {
+        var noPickEffect = "noPick"
+        var yesPickEffect = "yesPick"
+        
+        VStack(alignment: .center){
+            Spacer()
+            Text("Let's Talk About")
+                .font(Font.custom("Chinese Rocks", size: 100))
+                .foregroundColor(.black)
+            
+            HStack(alignment: .center){
+                Spacer()
+                Text("Rock?")
+                    .font(Font.custom("Chinese Rocks", size: 100))
+                    .foregroundColor(.black)
+                Spacer()
+            }
+            Spacer()
+                .frame(height: 150)
+            HStack(alignment: .center){
+                Image(uiImage: UIImage(named: "noPick")!)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 140, height: 140)
+                    .shadow(radius: isHoverNo ? 5 : 0)
+                    .animation(.spring())
+                    .onHover { hoverN in
+                        isHoverNo = hoverN
+                    }
+                Spacer()
+                    .frame(width: 80)
+                Button(action: {
+                    PlaygroundPage.current.navigateTo(page: .next)
+                }){
+                    Image(uiImage: UIImage(named: "yesPick")!)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 140, height: 140)
+                        .shadow(radius: isHoverYes ? 5 : 0)
+                        .animation(.spring())
+                        .onHover { hover in
+                            isHoverYes = hover
+                        }
+                }
+                
+            }
+            Spacer()
+            
+        }.background(Color(#colorLiteral(red: -0.15475499629974365, green: 0.66641765832901, blue: 0.5781239867210388, alpha: 1.0)))
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    
+}
+
+
+
