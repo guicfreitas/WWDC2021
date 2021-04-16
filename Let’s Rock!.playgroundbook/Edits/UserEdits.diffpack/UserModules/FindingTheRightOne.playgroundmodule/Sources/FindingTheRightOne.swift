5644a2681086b74518d97f428458f8a571e8b2ad
guilherme <jguicf@gmail.com>
2021-04-16 17:45:44 -0400

    feat: add new page

---
+++ Let’s Rock!.playgroundbook/Edits/UserEdits.diffpack/UserModules/FindingTheRightOne.playgroundmodule/Sources/FindingTheRightOne.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+import PlaygroundSupport
+
+public struct FindingTheRightOne : View{
+    public init(){
+        
+    }
+    
+    public var body : some View{
+        ZStack(){
+            
+        }.background(Color(#colorLiteral(red: 0.9501661658287048, green: 0.6621111035346985, blue: 0.22048801183700562, alpha: 1.0)))
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
