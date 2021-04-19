e9b6048d130fc7fee0a9a32f62e013ee8c875bb8
guilherme <jguicf@gmail.com>
2021-04-19 17:44:33 -0400

    feat: add markup

--- Let’s Rock!.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Page2.playgroundpage/main.swift
+++ Let’s Rock!.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Page2.playgroundpage/main.swift
@@ -1,6 +1,10 @@
 //: # Creating a Rhythm
 //: So let's start our journey! Here you will find out more about Rock, But you have to create a new rhythm with some existing genre fisrt.
 /*:
+ 
+  - callout(Gui):
+     📝 Editor's note: I was deep in research while looking for contents for this Playground. And I fell in love for blues, one of the rhythms that... hm, I’m not gonna give you a spoiler!
+  
  - Important:
     For a better experience, put your playground in full screen, close the side bar, make sure that's the only thing you see is the editor and the live view. Last but not least, enjoy this playground made with so much love <3
  */
