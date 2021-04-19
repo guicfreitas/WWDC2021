00895f9d14c679ee763b835b8cd36118c647ca3f
guilherme <jguicf@gmail.com>
2021-04-19 18:19:41 -0400

    fix: markups

--- Let’s Rock!.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Page2.playgroundpage/main.swift
+++ Let’s Rock!.playgroundbook/Contents/Chapters/Chapter1.playgroundchapter/Pages/Page2.playgroundpage/main.swift
@@ -4,7 +4,8 @@
  
   - callout(Gui):
      📝 Editor's note: I was deep in research while looking for contents for this Playground. And I fell in love for blues, one of the rhythms that... hm, I’m not gonna give you a spoiler!
-  
+ */
+/*:
  - Important:
     For a better experience, put your playground in full screen, close the side bar, make sure that's the only thing you see is the editor and the live view. Last but not least, enjoy this playground made with so much love <3
  */
